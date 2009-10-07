9bb3bae46dfcba63724966de5381e870b6ef638d
Devin Austin <dhoss@squishface.com>
2009-10-07 01:26:28 -0600

    initial commit

---
+++ Recursive.pm
@@ -0,0 +1,18 @@
+package DBIx::Class::Tree::Recursive;
+
+
+use Moose;
+extends 'DBIx::Class';
+
+## stuff!
+
+sub get_children {}
+
+sub add_child {}
+
+sub get_child {}
+
+sub get_parent {}
+
+
+1;
---
+++ lib/DBIx/Class/Tree/Recursive.pm
@@ -0,0 +1,8 @@
+package DBIx::Class::Tree::Recursive;
+
+
+use Moose;
+extends 'DBIx::Class';
+
+## stuff!
+1;
